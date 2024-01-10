162fe3dc906f226dfaaaca2bd1216df1615955fa
Manel <manelkld369@gmail.com>
2024-01-10 22:58:35 +0100

    premier commit

---
+++ regression.py
@@ -0,0 +1,9 @@
+#________________Préparation de l'environnement_____________
+#Installation des libs
+# python -m pip install pandas / python -m pip install -U matplotlib / python -m pip install plotly / pip install streamlit
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+
+#side bar
+st.sidebar.markdown('<p class="font">Regression</p>', unsafe_allow_html=True)
